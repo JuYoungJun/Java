cb318343da37627afef425aa07384c3658447829
mandoo <160207399+JuYoungJun@users.noreply.github.com>
2024-03-22 08:47:39 +0900

    Add files via upload

---
+++ JavaStudy/Chap02/src/TesPackage/Test.java
@@ -0,0 +1,18 @@
+package TesPackage;
+
+public class Test {
+
+	public static void main(String[] args) {
+		long var1 = 2L;
+		float var2 = 1.8f;
+		double var3 = 2.5;
+		String var4 = "3.9";
+		int result = (int)(var1+(var2+var3))+(int)Double.parseDouble(var4);
+		int result2 = (int)(var1+(int)var2+var3+Double.parseDouble(var4));
+		int result3 = (int)var1+(int)(var2+var3)+(int)Double.parseDouble(var4);
+		System.out.println("1번은 : " + result);
+		System.out.println("2번은 : " + result2);
+		System.out.println("3번은 : " + result3);
+		}
+
+}
